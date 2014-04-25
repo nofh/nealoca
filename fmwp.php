7122e703881c87f577c8a7b3ae41dc82dc5ceca8
dendev <dendev@new-host-2>
2014-04-25 07:13:35 +0000

    organisation

--- fmwp.php
+++ fmwp.php
@@ -5,8 +5,9 @@
  * A foundation off of which to build well-documented WordPress plugins that
  * also follow WordPress Coding Standards and PHP best practices.
  *
- * @package   organisme
- * @author    dendev <ddv@awt.be>
+ * @category Fmwp
+ * @package  Organisme
+ * @author   dendev <ddv@awt.be>
  *
  * @wordpress-plugin
  * Plugin Name:       FMWP
@@ -16,33 +17,34 @@
  * Author:            dendev
  * Text Domain:       fmwp_lang
  * Git Plugin URI:    https://github.com/nofh/fmwp.git
+ * @license           GPL2 
  */
+
+/*----------------------------------------------------------------------------*
+ * COMMON 
+ *----------------------------------------------------------------------------*/
+require_once plugin_dir_path(__FILE__) . 'config.php';
+require_once plugin_dir_path(__FILE__) . 'includes/CustomPostType.php';
+require_once plugin_dir_path(__FILE__) . 'includes/TestCPT.php'; 
 
 /*----------------------------------------------------------------------------*
  * FRONT
  *----------------------------------------------------------------------------*/
-require_once( plugin_dir_path( __FILE__ ) . 'config.php' );
-//commun
-require_once( plugin_dir_path( __FILE__ ) . 'includes/CustomPostType.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'includes/TestCPT.php' ); // possibilite de le mettre ds le front ?
-//admin
-//public
-require_once( plugin_dir_path( __FILE__ ) . 'public/FmWpFront.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'public/includes/ShortCode.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'public/includes/TestSHC.php' );
+require_once plugin_dir_path(__FILE__) . 'public/FmWpFront.php';
+require_once plugin_dir_path(__FILE__) . 'public/includes/ShortCode.php';
+require_once plugin_dir_path(__FILE__) . 'public/includes/TestSHC.php';
 
 
 // activation et desactivation
-register_activation_hook( __FILE__, array( 'OrganismeFront', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'OrganismeFront', 'deactivate' ) );
+register_activation_hook(__FILE__, array('OrganismeFront', 'activate'));
+register_deactivation_hook(__FILE__, array('OrganismeFront', 'deactivate'));
 
 // instancie le front
-add_action( 'plugins_loaded', array( 'OrganismeFront', 'get_instance' ) );
+add_action('plugins_loaded', array('OrganismeFront', 'get_instance'));
 /*----------------------------------------------------------------------------*
  * ADMIN
  *----------------------------------------------------------------------------*/
-if ( is_admin() ) 
-{
-	require_once( plugin_dir_path( __FILE__ ) . 'admin/FmWpAdmin.php' );
-	add_action( 'plugins_loaded', array( 'OrganismeAdmin', 'get_instance' ) );
+if (is_admin()) {
+	include_once plugin_dir_path(__FILE__) . 'admin/FmWpAdmin.php';
+	add_action('plugins_loaded', array('OrganismeAdmin', 'get_instance'));
 }
